3ff61a525c035748b4b6927f71400c564c766fff
Fahad Zafar <webdevelepor59@gmail.com>
2025-12-29 14:26:57 +0500

    In this file I learn the JavaScript
    I learn the Data types in JavaScript and Operators.

---
+++ script.js
@@ -0,0 +1,34 @@
+var num1 = 100
+var num2 ="papaya"
+console.log =(num1)
+console.log =(num2)
+
+var firstname ="fahad"
+var firstname ="ayan"
+console.log(firstname)
+
+let secondname ="zafar"
+let numberVar ="50"
+console.log(numberVar)
+numberVar =50
+console.log =(secondname)
+console.log =(numberVar)
+
+// /const
+const constantVar = 60
+//  constantVar = 70
+console.log(constantVar)
+
+
+    //   datatype
+
+         ////string
+   let strDataType =(mango) 
+   let strseconddatatype =(guava)
+   let sta =78
+   console.log=(strDataType , strseconddatatype , sta)
+    
+        //number
+
+        let numberVarrable=( let345)
+let num23 =3.4
