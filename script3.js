ee0ae4c6e1fdc2aa9612516050cc47e247a3a257
Fahad Zafar <webdevelepor59@gmail.com>
2025-12-30 14:59:23 +0500

    in this  i learnt about conditions

---
+++ script3.js
@@ -0,0 +1,20 @@
+let age1 = 20
+let age2 = 18
+
+if(age >= 22 && age2 >= 22){
+    console.log("now you can drive any vehicle")
+}
+
+else if(age >= 19 && age2 >= 16){
+    console.log("wait 2 or 1 year to drive any vehicle")
+}
+
+else if (age <= 12 && age2 <= 18){
+    console.log('Sorry you are a child play GTA Game')
+}
+
+else{
+    console.log("sorry you cannot drive any vehicle")
+    }
+
+
